a858b293d42fd104f0986490382ac86da0d33d4d
Maxim Schepelin <m.schepelin@gmail.com>
2015-08-15 19:00:14 +0300

    add method update to partical

--- swarm/particle.go
+++ swarm/particle.go
@@ -17,17 +17,24 @@ func random(min, max float64) float64 {
 	return rand.Float64()*(max-min) + min
 }
 
-func (p *Particle) initialize(variablesLength int, minValue float64, maxValue float64) {
+func (p *Particle) initialize(length int, minValue float64, maxValue float64) {
+	p.position = make([]float64, length)
+	p.bestPosition = make([]float64, length)
+	p.velocity = make([]float64, length)
 
-	p.position = make([]float64, variablesLength)
-	p.bestPosition = make([]float64, variablesLength)
-	p.velocity = make([]float64, variablesLength)
-
-	for i := 0; i < variablesLength; i++ {
+	for i := 0; i < length; i++ {
 		value := random(minValue, maxValue)
 
 		p.bestPosition[i] = value
 		p.position[i] = value
 	}
+}
 
+func (p *Particle) update(swarmBestPosition []float64, swarmPriority float64, individualPriority float64) {
+	for i, value := range p.velocity {
+		p.velocity[i] = value +
+			swarmPriority*rand.Float64()*(p.bestPosition[i]-p.position[i]) +
+			individualPriority*rand.Float64()*(swarmBestPosition[i]-p.position[i])
+		p.position[i] = p.position[i] + p.velocity[i]
+	}
 }
